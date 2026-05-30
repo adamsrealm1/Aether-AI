d0ffeacf68f225868160167aa3ac40910399f5ab
Star <adamsrealm1@gmail.com>
2026-05-30 18:43:22 -0400

    hCaptcha implementation; advanced security added

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.AETHER_API_ENDPOINT = "https://aether-ai.wasmer.app";
+window.AETHER_API_ENDPOINT = "https://aetherai.wasmer.app";
 window.AETHER_HCAPTCHA_SITE_KEY = "d80ab67c-f262-4b58-834c-fab69e447f88";
